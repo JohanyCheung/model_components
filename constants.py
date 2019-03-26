cc557f8752d6e33f688f9078b7c342049845fd75
JohanyCheung <1979450016@qq.com>
2019-03-27 04:21:39 +0800

    update

--- constants.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-import os
-
-# Local file system
-ROOT_PATH = os.path.split(os.path.abspath(__file__))[0].replace("\\", "/") + "/"
-DATA_PATH = ROOT_PATH + "datasets/"
-LOG_PATH = ROOT_PATH + "log/"
